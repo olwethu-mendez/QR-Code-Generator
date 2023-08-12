268db3d340f3b2d0ba5c47c7e56f767aea739c8f
Olwethu Mzileni <s220428336@mandela.ac.za>
2023-08-12 06:19:53 +0200

    changed font & added save button.

--- script/script.js
+++ script/script.js
@@ -2,7 +2,8 @@ const card = document.querySelector(".card"),
     generateBtn = card.querySelector(".form button"),
     qrImg = card.querySelector(".qr-code img"),
     qrInput = card.querySelector(".form input"),
-    closeQr = card.querySelector(".close-qr");
+    closeQr = card.querySelector(".close-qr"),
+    qrDownload = document.querySelector(".downloadQr");
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
@@ -24,4 +25,36 @@ qrInput.addEventListener("keyup", () => {
     if(!qrInput.value){
         card.classList.remove("active");
     }
+});
+
+qrDownload.addEventListener("click", () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let MM = today.getMonth() + 1;
+    let dd = today.getDate();
+    let hh = today.getHours();
+    let mm = today.getMinutes();
+    let ss = today.getSeconds();
+
+    if(ss < 10) ss = '0' + ss;
+    if(mm < 10) mm = '0' + mm;
+    if(hh < 10) hh = '0' + hh;
+    if(dd < 10) dd = '0' + dd;
+    if(MM < 10) MM = '0' + MM;
+
+    const formattedTime = yyyy + '-' + MM + '-' + dd + '-' + hh + '-' + mm + '-' + ss;
+   
+    let imageName = "qr-image_" + formattedTime.toString() + ".png";
+    
+    function saveImage(imageLink, name) {
+        fetch(imageLink).then((response) => {
+            return response.blob().then((b) =>{
+                const link = document.createElement('a');
+                link.setAttribute("download", name);
+                link.href = URL.createObjectURL(b);
+                link.click();
+            })
+        })
+    }
+    saveImage(qrImg.src,imageName)
 })
